3a6ecbdd9f86eef9e54cb0e5235119656fd02676
guyrefaeli1212-cell <guyrefaeli1212@gmail.com>
2026-08-02 21:59:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ else:
                 שאלה/הודעה: {user_input}
                 """
                 response = client.models.generate_content(
-                model='gemini-2.0-flash',
+                model='gemini-1.5-flash',
                     contents=prompt
                 )
                 bot_reply = response.text
